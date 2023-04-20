49d60d98d6a987c7f0cd6dc1f51cfe9d503a6442
Lais Faustino <laisfaustino@hotmail.com>
2023-04-20 17:20:21 -0300

    Update ProdutoRepository.java

--- src/main/java/com/generation/farmacia/repository/ProdutoRepository.java
+++ src/main/java/com/generation/farmacia/repository/ProdutoRepository.java
@@ -4,30 +4,18 @@ import java.math.BigDecimal;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.generation.farmacia.model.Produto;
 
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
-	public List<Produto> findAllByNomeContainingIgnoreCase(String nome);
+	public List <Produto> findAllByNomeContainingIgnoreCase(@Param("nome") String nome);
 
-	/**
-	 *  Método Personalizado - Buscar todos os Produtos cujo o preço seja maior 
-	 *  do que um valor digitado ordenado pelo preço em ordem crescente
-	 *  
-	 *  MySQL: select * from tb_produtos where preco > preco order by preco;
-	 */
-	 
-	public List <Produto> findAllByPrecoGreaterThanOrderByPreco(BigDecimal preco);
-	
-	/**
-	 *  Método Personalizado - Buscar todos os Produtos cujo o preço seja menor 
-	 *  do que um valor digitado ordenado pelo preço em ordem decrescente
-	 *  
-	 *  MySQL: select * from tb_produtos where preco < preco order by preco desc;
-	 */
-	 
-	public List <Produto> findAllByPrecoLessThanOrderByPrecoDesc(BigDecimal preco);
-	
+	public List <Produto> findByNomeOrLaboratorioIgnoreCase(@Param("nome") String nome,@Param("laboratorio") String laboratorio);
+
+	public List <Produto> findByNomeAndLaboratorioIgnoreCase(@Param("nome") String nome,@Param("laboratorio") String laboratorio);
+
+	public List <Produto> findByPrecoBetween(BigDecimal inicio, BigDecimal fim);
 }
 
 
